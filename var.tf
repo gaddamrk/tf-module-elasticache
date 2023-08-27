21032fd528351781278bb4568e680aabc409db74
gaddamrk <raj>
2023-08-27 13:38:20 +0530

    jr

---
+++ var.tf
@@ -0,0 +1,7 @@
+variable "env" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
+variable "replicas_per_node_group" {}
+variable "node_type" {}
+variable "num_node_groups" {}
